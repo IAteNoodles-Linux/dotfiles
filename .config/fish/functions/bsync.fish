1c8400fa0d00429a17c5673385277e593ab44c15
IAteNoodles <iatenoodles.linux@protonmail.com>
2022-05-14 18:20:53 +0530

    Daily commit using a script

--- .config/fish/functions/bsync.fish
+++ .config/fish/functions/bsync.fish
@@ -1,19 +1,3 @@
 function bsync
-cd ~/.cache/yay/
-set zstLoc (fd -a .zst)
-set zstName (cat ~/.cache/own_/bsync_names)
-for binary in $zstLoc
-set found "0"
-for name in $zstName
-if test "$binary"=="$name"
-set found "1"
-break
-end
-end
-if test "$found"=="0"
-sha1sum $binary >> ~/.cache/own_/bsync_sha1sum #Not sure, but saving the checksum
-echo $binary >> ~/.cache/own_/bsync_names 
-cp --verbose $binary /media/sdb5/New/
-end
-end
+sh $HOME/Scripts/sync_binaries.sh 
 end
